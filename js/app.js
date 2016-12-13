50ee0a75227d63c7ed45fb24401010a18b13a1b1
Steph-6 <Steph.robinson95@gmail.com>
2016-12-13 01:19:58 +0000

    monday night commit

--- js/app.js
+++ js/app.js
@@ -1,74 +1,78 @@
-var playing = true;
-var colors  = ['blue', 'green', 'purple'];
-var lives   = ['*****', '****', '***', '**', '*', '']
+var colors  = ['url(images/leaf.png)', 'url(images/bomb.jpg)', 'url(images/apple.jpeg)'];
+var color   = colors[0];
+var timer   = 1200;
 var score   = 0;
+var combo   = 0;
+var i       = 0;
+var $square;
+
 
 $(function() {
-  color = colors[0];
+  var $click = $('#click');
+  setInterval (function() {
 
-  // if (30 > score >= 20) {
-  //   counter = 20;
-  //   color = colors[chooseSquare(0,1)];
-  // }
-  // if (score >= 30) {
-  //   counter = 20;
-  //   color = colors[chooseSquare(0,2)];
-  // }
-
-  highlight(color);
-  move(color);
-
-  var playRow = document.getElementsByClassName('play-row');
-  for (var i = 0; i < playRow.length; i++) {
-    playRow[i].addEventListener('click', function() {
-      this.style.backgroundColor = 'yellow';
-      //audio
-    });
-  }
-
-  var bottomGrid = document.getElementsByClassName('row6');
-  console.log(bottomGrid);
-  for (var i = 0; i < bottomGrid.length; i++) {
-    if (bottomGrid[i].style.backgroundColor !== 'yellow') {
-      alert('You lose');
-      //move the array up
-      //document.getElementById('lives').innerHTML = lives--;
-      //audio
-      //clearInterval(highlight(color));
-    } else {
-      score++;
-      document.getElementById('score').innerHTML = score;
+    if (20 > score >= 10) {
+      timer = 900;
+      color = colors[chooseSquare(0,1)];
     }
-  }
-});
+    if (score >= 20) {
+      timer = 700;
+      color = colors[chooseSquare(0,2)];
+    }
+    if (score < 0) {
+      $('h3').html('Game Over!');
+      $('#score').html('ZEROO');
+      return;
+    }
 
-function highlight(color) {
-  var lis = document.getElementsByTagName('li');
-  for (var i = 0; i < lis.length; i++) {
-    lis[i].style.backgroundColor = 'yellow';
-  }
-  document.getElementById('grid1.' + chooseSquare(1,4)).style.backgroundColor = color;
-}
+    $square = $('<div/>', {
+      'id': 'square',
+       css: {'background-image': color}
+    });
+
+    $('#column' + chooseSquare(1,4)).append($square);
+    $square.animate(
+      {top: '+=492px'},
+      {duration: timer,
+         //easing: 'easeInOut',
+         easing: 'linear',
+       complete: function() {
+        $(this).remove();
+      }
+    });
+
+    $square.on('click', function() {
+      if (60 < ($square.parent().offset().top + $square.parent()[0].offsetHeight - $square.offset().top) < 420 && color !== colors[1]) {
+        score += 2;
+        $('#score').html(score);
+        $square.css('background-image', 'url(images/bang.jpg)');
+        combo++;
+        $('#combo').html('Combo = ' + combo);
+        console.log(combo);
+        $('#click').get(0).play();
+      }
+    });
+  }, timer-300);
+
+  $('.column').on('click', function(e) {
+    if(e.target == e.currentTarget){
+      score--;
+      $('#score').html(score);
+      // lives = lives[0];
+      // console.log(lives);
+      combo = 0;
+      $('#combo').html('Combo = ' + combo);
+    }
+  })
+
+  $('button').on('click', function(){
+    location.reload();
+  });
+
+});
 
 function chooseSquare(a, b) {
   min = Math.ceil(a);
   max = Math.floor(b);
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
-function move(color){
-  setInterval(function() { highlight(color) }, 500);
-  for (var i = 1; i < 7; i++) {
-    var rows = document.getElementsByClassName('row' + i);
-    console.log(rows);
-    //for (var j = 0; j < rows.length; j++) {
-      //rows[j++].style.backgroundColor = rows[j].style.backgroundColor;
-    //}
-  }
-}
-
-// if (score > 20) {
-//   counter = 20;
-//   var colors = ['blue', 'green', 'purple'];
-//   color = colors[chooseSquare(0,3)];
-// }
